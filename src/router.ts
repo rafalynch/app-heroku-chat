c857ed82fdea2f799f768d2409343b34e4db8063
Rafa Lynch <rafa_lynch@hotmail.com>
2021-09-06 23:24:47 -0300

    redirect

--- src/router.ts
+++ src/router.ts
@@ -4,6 +4,6 @@ const outlet = document.querySelector(".root");
 const router = new Router(outlet);
 router.setRoutes([
   { path: "/home", component: "custom-home" },
-  { path: "/", component: "custom-home" },
+  { path: "/", redirect: "/home" },
   { path: "/chat", component: "custom-chat" },
 ]);
